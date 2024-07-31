3bc911ed99fe3d48de29e809fbba3ac18cd47cf4
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-31 12:53:40 -0400

    addeventlistener added and fixed some randomcolors functions problems

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-// Kodlar buraya
+const backgroundColor = document.querySelector(".bgDiv");
 
-const backgroundColor = document.querySelectorAll(".bgDiv");
+const buttonClick = document.querySelector(".btn-click");
 
-const buttonClick=document.querySelectorAll(".btn-click");
+buttonClick.addEventListener("click", function () {
+  const newColor = randomColors();
+  backgroundColor.style.backgroundColor = newColor;
+});
 
-buttonClick.addEventListener('click', listener, options)
+function randomColors() {
+  const colorLetters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += colorLetters[Math.floor(Math.random() * 16)]; // *16 ile carpmak sifir ile 15 arasinda bir sayi secer 15 dahil ! *100 deseydik 100e kadar secicekti.
+  }
 
- function randomColors(item) { 
-    
-    const colorLetters= '0123456789ABCDEF';
-    let color = "#";
-    for(let i=0; i>14; i++) {
-
-        color += colorLetters.[Math.floor(math.random() *15 )];  // *15 ile carpmak sifir ile 15 arasinda bir sayi secer 15 dahil ! *100 deseydik 100e kadar secicekti.
-    }
-    
-
-    return color;
-    
-   
-    
+  return color;
 }
