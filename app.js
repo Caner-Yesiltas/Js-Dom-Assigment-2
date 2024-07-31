14e2d15d864ef569b782df499e7c5414f4961dea
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-31 11:44:35 -0400

    random colors function added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Kodlar buraya
+
+const backgroundColor = document.querySelectorAll(".bgDiv");
+
+const buttonClick=document.querySelectorAll(".btn-click");
+
+buttonClick.addEventListener('click', listener, options)
+
+ function randomColors(item) { 
+    
+    const colorLetters= '0123456789ABCDEF';
+    let color = "#";
+    for(let i=0; i>14; i++) {
+
+        color += colorLetters.[Math.floor(math.random() *15 )];  // *15 ile carpmak sifir ile 15 arasinda bir sayi secer 15 dahil ! *100 deseydik 100e kadar secicekti.
+    }
+    
+
+    return color;
+    
+   
+    
+}
